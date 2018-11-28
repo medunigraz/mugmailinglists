b3f8542253185bf623e5989f99420550c954bf80
Gerhard Janisch <Gerhard.Janisch@medunigraz.at>
2018-11-28 14:36:11 +0100

    ldap settings added

--- src/mugmailinglists/settings.py
+++ src/mugmailinglists/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import ldap
+import saml2
+import saml2.attributemaps
+import saml2.saml
+from corsheaders.defaults import default_methods
+from django.utils.translation import ugettext_lazy as _
+from django_auth_ldap.config import (
+    GroupOfNamesType,
+    LDAPSearch,
+)
+from docutils.core import publish_parts
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -89,6 +101,134 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mugmailinglists.wsgi.application'
 
 
+SAML_CREATE_UNKNOWN_USER = True
+SAML_ATTRIBUTE_MAPPING = {
+    'uid': ('username', ),
+}
+SAML_LOGOUT_REQUEST_PREFERRED_BINDING = saml2.BINDING_HTTP_POST
+SAML_CONFIG = {
+    # full path to the xmlsec1 binary programm
+    'xmlsec_binary': '/usr/bin/xmlsec1',
+
+    # your entity id, usually your subdomain plus the url to the metadata view
+    'entityid': 'http://localhost:8000/saml2/metadata/',
+
+    # directory with attribute mapping
+    'attribute_map_dir': os.path.dirname(saml2.attributemaps.__file__),
+
+    # this block states what services we provide
+    'service': {
+        # we are just a lonely SP
+        'sp': {
+            'name': 'Federated Django sample SP',
+            'name_id_format': saml2.saml.NAMEID_FORMAT_PERSISTENT,
+            'endpoints': {
+                # url and binding to the assetion consumer service view
+                # do not change the binding or service name
+                'assertion_consumer_service': [
+                    (
+                        'http://localhost:8000/saml2/acs/',
+                        saml2.BINDING_HTTP_POST
+                    ),
+                ],
+                # url and binding to the single logout service view
+                # do not change the binding or service name
+                'single_logout_service': [
+                    (
+                        'http://localhost:8000/saml2/ls/',
+                        saml2.BINDING_HTTP_REDIRECT
+                    ),
+                    (
+                        'http://localhost:8000/saml2/ls/post',
+                        saml2.BINDING_HTTP_POST
+                    ),
+                ],
+            },
+
+            # attributes that this project need to identify a user
+            'required_attributes': ['uid'],
+
+            # attributes that may be useful to have but not required
+            'optional_attributes': ['eduPersonAffiliation'],
+
+            # in this section the list of IdPs we talk to are defined
+            'idp': {
+                # we do not need a WAYF service since there is
+                # only an IdP defined here. This IdP should be
+                # present in our metadata
+
+                # the keys of this dictionary are entity ids
+                'https://localhost/simplesaml/saml2/idp/metadata.php': {
+                    'single_sign_on_service': {
+                        saml2.BINDING_HTTP_REDIRECT: 'https://localhost/simplesaml/saml2/idp/SSOService.php',
+                    },
+                    'single_logout_service': {
+                        saml2.BINDING_HTTP_REDIRECT: 'https://localhost/simplesaml/saml2/idp/SingleLogoutService.php',
+                    },
+                },
+            },
+        },
+    },
+
+    # where the remote metadata is stored
+    'metadata': {
+        'local': [os.path.join(BASE_DIR, 'remote_metadata.xml')],
+    },
+
+    # set to 1 to output debugging information
+    'debug': 1,
+
+    # Signing
+    'key_file': os.path.join(BASE_DIR, 'mycert.key'),  # private part
+    'cert_file': os.path.join(BASE_DIR, 'mycert.pem'),  # public part
+
+    # Encryption
+    'encryption_keypairs': [{
+        'key_file': os.path.join(BASE_DIR, 'my_encryption_key.key'),  # private part
+        'cert_file': os.path.join(BASE_DIR, 'my_encryption_cert.pem'),  # public part
+    }],
+
+    # own metadata settings
+    'contact_person': [
+        {
+            'given_name': 'Lorenzo',
+            'sur_name': 'Gil',
+            'company': 'Yaco Sistemas',
+            'email_address': 'lgs@yaco.es',
+            'contact_type': 'technical'
+        },
+        {
+            'given_name': 'Angel',
+            'sur_name': 'Fernandez',
+            'company': 'Yaco Sistemas',
+            'email_address': 'angel@yaco.es',
+            'contact_type': 'administrative'
+        },
+    ],
+    # you can set multilanguage information here
+    'organization': {
+        'name': [
+            ('Yaco Sistemas', 'es'),
+            ('Yaco Systems', 'en'),
+        ],
+        'display_name': [
+            ('Yaco', 'es'),
+            ('Yaco', 'en'),
+        ],
+        'url': [
+            ('http://www.yaco.es', 'es'),
+            ('http://www.yaco.com', 'en'),
+        ],
+    },
+    'valid_for': 24,  # how long is our metadata valid
+}
+
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+    'djangosaml2.backends.Saml2Backend',
+)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -106,6 +246,38 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+AUTH_LDAP_SERVER_URI = "ldap://ldap.example.com"
+AUTH_LDAP_BIND_DN = "cn=django-agent,dc=example,dc=com"
+AUTH_LDAP_BIND_PASSWORD = "phlebotinum"
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=example,dc=com", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=django,ou=groups,dc=example,dc=com",
+    ldap.SCOPE_SUBTREE, "(objectClass=groupOfNames)"
+)
+AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")
+AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=django,ou=groups,dc=example,dc=com"
+AUTH_LDAP_DENY_GROUP = "cn=disabled,ou=django,ou=groups,dc=example,dc=com"
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail"
+}
+AUTH_LDAP_PROFILE_ATTR_MAP = {
+    "employee_number": "employeeNumber"
+}
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    "is_active": "cn=active,ou=django,ou=groups,dc=example,dc=com",
+    "is_staff": "cn=staff,ou=django,ou=groups,dc=example,dc=com",
+    "is_superuser": "cn=superuser,ou=django,ou=groups,dc=example,dc=com"
+}
+AUTH_LDAP_PROFILE_FLAGS_BY_GROUP = {
+    "is_awesome": "cn=awesome,ou=django,ou=groups,dc=example,dc=com",
+}
+AUTH_LDAP_ALWAYS_UPDATE_USER = True
+AUTH_LDAP_FIND_GROUP_PERMS = True
+AUTH_LDAP_CACHE_GROUPS = True
+AUTH_LDAP_GROUP_CACHE_TIMEOUT = 3600
 
 
 # Internationalization
